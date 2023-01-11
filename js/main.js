a9fbdb6b464b18ae6147a3fd6adf21c387d6e0e4
c-e-l-i-a <celia_torno@yahoo.com>
2023-01-11 16:35:36 +0100

    Second-commit img api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import {MY_APP_KEY} from './config.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+	fetchImage()
+})
 const options = {
 	method: 'GET',
 	headers: {
@@ -8,12 +11,15 @@ const options = {
 	}
 };
 
+
 fetch('https://k-pop.p.rapidapi.com/boy-groups/random', options)
 	.then(response => response.json())
 	.then(response => { console.log(response);
 
+		
+
     document.getElementById('name_search').innerHTML = response.data[0]['Group Name']
-    
+    document.getElementById('the_img').scr = response.data[0]['Group Profile']
     document.getElementById('date_debut').innerHTML = 'Date of debute : ' + response.data[0]['Date of Debut']
     document.getElementById('name_company').innerHTML = 'Company name : ' + response.data[0].Company
     document.getElementById('active_situation').innerHTML = 'Active : ' + response.data[0].Active
@@ -23,3 +29,14 @@ fetch('https://k-pop.p.rapidapi.com/boy-groups/random', options)
 	.catch(err => {
         console.error(err)
     });
+
+	
+	function fetchImage() {
+		let image = document.getElementById('the_img')
+		fetch('https://k-pop.p.rapidapi.com/boy-groups/random', options)
+		.then(response => response.json())
+		.then(response => image.scr = response.data[0]['Group Profile'])	
+	}
+
+	
+
